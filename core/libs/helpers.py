f54f2dd84721a72114f65c9d61570e1e4058546e
K4n1shk <k4n1shk1501@gmail.com>
2023-01-25 22:25:20 +0530

    Added the missing APIs

---
+++ core/libs/helpers.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+TIMESTAMP_WITH_TIMEZONE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f%z'
+
+
+class GeneralObject:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+def get_utc_now():
+    return datetime.utcnow()
